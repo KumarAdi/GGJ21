19c078a1b246b7e3996f19020ad9a27599b546e7
Aditya Kumar <adikumar@cs.washington.edu>
2021-01-31 18:37:57 -0800

    minify final build

--- docs/html5/haxe/ApplicationMain.hx
+++ docs/html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "71";
+		app.meta["build"] = "72";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "FlxProject";
 		app.meta["name"] = "FlxProject";
